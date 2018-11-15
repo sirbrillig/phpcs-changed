c03de39b59f8beae2fc2d75fd3ad8ba7ae07afc7
Payton Swick <payton@foolord.com>
2018-11-15 13:32:03 -0500

    Add getNewPhpcsOutput and test

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 declare(strict_types=1);
 
+namespace PhpcsDiff;
+
+use PhpcsDiff\DiffLineMap;
+
 require_once __DIR__ . '/PhpcsDiff/DiffLine.php';
 require_once __DIR__ . '/PhpcsDiff/DiffLineType.php';
 require_once __DIR__ . '/PhpcsDiff/DiffLineMap.php';
+
+function getNewPhpcsOutput($unifiedDiff, $oldPhpcsMessages, $newPhpcsMessages): array {
+	$map = DiffLineMap::fromUnifiedDiff($unifiedDiff);
+	return array_values(array_filter($newPhpcsMessages, function($newMessage) use ($oldPhpcsMessages, $map) {
+		$lineNumber = $newMessage['line'] ?? null;
+		if (! $lineNumber) {
+			return true;
+		}
+		$oldLineNumber = $map->getOldLineNumberForLine($lineNumber);
+		return count(array_values(array_filter($oldPhpcsMessages, function($oldMessage) use ($oldLineNumber) {
+			return ($oldMessage['line'] ?? null) === $oldLineNumber;
+		})) > 0);
+	}));
+}
---
+++ tests/functionsTest.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/index.php';
+
+use PHPUnit\Framework\TestCase;
+use function PhpcsDiff\getNewPhpcsOutput;
+
+final class PhpcsDiffTest extends TestCase {
+	public function testFirstThing() {
+		$diff = <<<EOF
+Index: review-stuck-orders.php
+===================================================================
+--- bin/review-stuck-orders.php	(revision 183265)
++++ bin/review-stuck-orders.php	(working copy)
+@@ -17,6 +17,7 @@
+ use Billing\Purchases\Order;
+ use Billing\Services;
+ use Billing\Ebanx;
++use Foobar;
+ use Billing\Emergent;
+ use Billing\Monetary_Amount;
+ use Stripe\Error;
+EOF;
+		$oldFilePhpcs = [
+			[ 'line' => 20 ],
+			[ 'line' => 99 ],
+			[ 'line' => 108 ],
+			[ 'line' => 111 ],
+			[ 'line' => 114 ],
+		];
+		$newFilePhpcs = [
+			[ 'line' => 20 ],
+			[ 'line' => 21 ],
+			[ 'line' => 100 ],
+			[ 'line' => 109 ],
+			[ 'line' => 112 ],
+			[ 'line' => 115 ],
+		];
+		$actual = getNewPhpcsOutput($diff, $oldFilePhpcs, $newFilePhpcs);
+		$expected = [
+			[ 'line' => 20 ],
+		];
+		$this->assertEquals($expected, $actual);
+	}
+}
