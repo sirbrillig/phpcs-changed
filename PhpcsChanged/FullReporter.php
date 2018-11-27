55ee4ff1e902411cec2fbfbf7fcb3a038b0e4b23
Payton Swick <payton@foolord.com>
2018-11-26 20:52:17 -0500

    Move full formatting to Reporter

--- PhpcsChanged/FullReporter.php
+++ PhpcsChanged/FullReporter.php
@@ -5,10 +5,38 @@ namespace PhpcsChanged;
 
 use PhpcsChanged\Reporter;
 use PhpcsChanged\PhpcsMessages;
+use PhpcsChanged\PhpcsMessage;
+use function PhpcsChanged\Cli\getLongestString;
 
 class FullReporter implements Reporter {
 	public function getFormattedMessages(PhpcsMessages $messages): string {
-		return $messages->toFullOutput() . PHP_EOL;
+		$file = isset($messages->getMessages()[0]) ? $messages->getMessages()[0]->getFile() : 'STDIN';
+		$errorsCount = count(array_values(array_filter($messages->getMessages(), function($message) {
+			return $message->getType() === 'ERROR';
+		})));
+		$warningsCount = count(array_values(array_filter($messages->getMessages(), function($message) {
+			return $message->getType() === 'WARNING';
+		})));
+		$lineCount = count($messages->getMessages());
+		$linePlural = ($lineCount === 1) ? '' : 'S';
+		$errorPlural = ($errorsCount === 1) ? '' : 'S';
+		$warningPlural = ($warningsCount === 1) ? '' : 'S';
+		$longestNumber = getLongestString(array_map(function(PhpcsMessage $message): int {
+			return $message->getLineNumber();
+		}, $messages->getMessages()));
+		$formattedLines = implode("\n", array_map(function(PhpcsMessage $message) use ($longestNumber): string {
+			return sprintf(" %{$longestNumber}d | %s | %s", $message->getLineNumber(), $message->getType(), $message->getMessage());
+		}, $messages->getMessages()));
+		return <<<EOF
+
+FILE: {$file}
+-----------------------------------------------------------------------------------------------
+FOUND {$errorsCount} ERROR{$errorPlural} AND {$warningsCount} WARNING{$warningPlural} AFFECTING {$lineCount} LINE{$linePlural}
+-----------------------------------------------------------------------------------------------
+{$formattedLines}
+-----------------------------------------------------------------------------------------------
+
+EOF;
 	}
 
 	public function getExitCode(PhpcsMessages $messages): int {
