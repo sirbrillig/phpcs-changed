a14331267abbcd02c4e73aed128fa1fcfa533b6e
Payton Swick <payton@foolord.com>
2019-02-28 14:41:35 -0500

    Add new loaders

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ require_once __DIR__ . '/PhpcsChanged/Cli.php';
 require_once __DIR__ . '/PhpcsChanged/Reporter.php';
 require_once __DIR__ . '/PhpcsChanged/JsonReporter.php';
 require_once __DIR__ . '/PhpcsChanged/FullReporter.php';
+require_once __DIR__ . '/PhpcsChanged/NonFatalException.php';
+require_once __DIR__ . '/PhpcsChanged/SvnWorkflow.php';
 
 function getNewPhpcsMessages(string $unifiedDiff, PhpcsMessages $oldPhpcsMessages, PhpcsMessages $newPhpcsMessages): PhpcsMessages {
 	$map = DiffLineMap::fromUnifiedDiff($unifiedDiff);
