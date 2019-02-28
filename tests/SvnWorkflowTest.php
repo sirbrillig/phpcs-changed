3ce22d7db9cabada0792e485af221dbb24e77ee8
Payton Swick <payton@foolord.com>
2019-02-28 14:52:23 -0500

    Add tests for getSvnUnifiedDiff

--- tests/SvnWorkflowTest.php
+++ tests/SvnWorkflowTest.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 require_once dirname(__DIR__) . '/index.php';
 
 use PHPUnit\Framework\TestCase;
-use function PhpcsChanged\SvnWorkflow\isNewSvnFile;
+use PhpcsChanged\NonFatalException;
+use function PhpcsChanged\SvnWorkflow\{isNewSvnFile, getSvnUnifiedDiff};
 
 final class SvnWorkflowTest extends TestCase {
    public function testIsNewSvnFileReturnsTrueForNewFile() {
@@ -55,6 +56,47 @@ Checksum: abcdefg
       };
       $debug = function($message) {}; //phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
       $this->assertFalse(isNewSvnFile($svnFile, $svn, $executeCommand, $debug));
+   }
+
+   public function testGetSvnUnifiedDiff() {
+      $svnFile = 'foobar.php';
+      $svn = 'svn';
+      $diff = <<<EOF
+Index: foobar.php
+===================================================================
+--- bin/foobar.php	(revision 183265)
++++ bin/foobar.php	(working copy)
+@@ -17,6 +17,7 @@
+ use Billing\Purchases\Order;
+ use Billing\Services;
+ use Billing\Ebanx;
++use Foobar;
+ use Billing\Emergent;
+ use Billing\Monetary_Amount;
+ use Stripe\Error;
+EOF;
+      $executeCommand = function($command) use ($diff) {
+	 if (! $command || false === strpos($command, "svn diff 'foobar.php'")) {
+	    return '';
+	 }
+	 return $diff;
+      };
+      $debug = function($message) {}; //phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+      $this->assertEquals($diff, getSvnUnifiedDiff($svnFile, $svn, $executeCommand, $debug));
+   }
+
+   public function testGetSvnUnifiedDiffThrowsNonFatalIfDiffFails() {
+      $this->expectException(NonFatalException::class);
+      $svnFile = 'foobar.php';
+      $svn = 'svn';
+      $executeCommand = function($command) {
+	 if (! $command || false === strpos($command, "svn diff 'foobar.php'")) {
+	    return 'foobar';
+	 }
+	 return '';
+      };
+      $debug = function($message) {}; //phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+      getSvnUnifiedDiff($svnFile, $svn, $executeCommand, $debug);
    }
 }
 
