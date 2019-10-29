333988bd9caccc5a73578860949f0e2f268b91a9
Payton Swick <payton@foolord.com>
2019-10-29 12:28:21 -0400

    Bump version to 2.2.7-beta-1

--- PhpcsChanged/functions.php
+++ PhpcsChanged/functions.php
@@ -8,7 +8,7 @@ use PhpcsChanged\PhpcsMessages;
 use PhpcsChanged\ShellException;
 
 function getVersion(): string {
-	return '2.2.6';
+	return '2.2.7-beta-1';
 }
 
 function getNewPhpcsMessages(string $unifiedDiff, PhpcsMessages $oldPhpcsMessages, PhpcsMessages $newPhpcsMessages): PhpcsMessages {
