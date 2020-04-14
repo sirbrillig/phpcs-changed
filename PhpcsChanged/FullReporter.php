bfb7e7e8993a6984db1db5b5458c5c5461a529a8
Payton Swick <payton@foolord.com>
2020-04-13 20:32:08 -0400

    Modify FullReporter to handle multiple files

--- PhpcsChanged/FullReporter.php
+++ PhpcsChanged/FullReporter.php
@@ -10,26 +10,50 @@ use function PhpcsChanged\Cli\getLongestString;
 
 class FullReporter implements Reporter {
 	public function getFormattedMessages(PhpcsMessages $messages): string {
-		$file = isset($messages->getMessages()[0]) ? $messages->getMessages()[0]->getFile() ?? 'STDIN' : 'STDIN';
-		$errorsCount = count(array_values(array_filter($messages->getMessages(), function($message) {
-			return $message->getType() === 'ERROR';
-		})));
-		$warningsCount = count(array_values(array_filter($messages->getMessages(), function($message) {
-			return $message->getType() === 'WARNING';
-		})));
+		$files = array_map(function(PhpcsMessage $message): string {
+			return $message->getFile() ?? 'STDIN';
+		}, $messages->getMessages());
+		if (empty($files)) {
+			$files = ['STDIN'];
+		}
+
 		$lineCount = count($messages->getMessages());
-		$linePlural = ($lineCount === 1) ? '' : 'S';
-		$errorPlural = ($errorsCount === 1) ? '' : 'S';
-		$warningPlural = ($warningsCount === 1) ? '' : 'S';
-		$longestNumber = getLongestString(array_map(function(PhpcsMessage $message): int {
-			return $message->getLineNumber();
-		}, $messages->getMessages()));
-		$formattedLines = implode("\n", array_map(function(PhpcsMessage $message) use ($longestNumber): string {
-			return sprintf(" %{$longestNumber}d | %s | %s", $message->getLineNumber(), $message->getType(), $message->getMessage());
-		}, $messages->getMessages()));
 		if ($lineCount < 1) {
 			return '';
 		}
+
+		return implode("\n", array_filter(array_map(function(string $file) use ($messages): ?string {
+			$messagesForFile = array_values(array_filter($messages->getMessages(), function(PhpcsMessage $message) use ($file): bool {
+				return $message->getFile() === $file;
+			}));
+			return $this->getFormattedMessagesForFile($messagesForFile, $file);
+		}, $files)));
+	}
+
+	private function getFormattedMessagesForFile(array $messages, string $file): ?string {
+		$lineCount = count($messages);
+		if ($lineCount < 1) {
+			return null;
+		}
+		$errorsCount = count(array_values(array_filter($messages, function($message) {
+			return $message->getType() === 'ERROR';
+		})));
+		$warningsCount = count(array_values(array_filter($messages, function($message) {
+			return $message->getType() === 'WARNING';
+		})));
+
+		$linePlural = ($lineCount === 1) ? '' : 'S';
+		$errorPlural = ($errorsCount === 1) ? '' : 'S';
+		$warningPlural = ($warningsCount === 1) ? '' : 'S';
+
+		$longestNumber = getLongestString(array_map(function(PhpcsMessage $message): int {
+			return $message->getLineNumber();
+		}, $messages));
+
+		$formattedLines = implode("\n", array_map(function(PhpcsMessage $message) use ($longestNumber): string {
+			return sprintf(" %{$longestNumber}d | %s | %s", $message->getLineNumber(), $message->getType(), $message->getMessage());
+		}, $messages));
+
 		return <<<EOF
 
 FILE: {$file}
