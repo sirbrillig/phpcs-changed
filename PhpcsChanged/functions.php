e3522fbceeebd44f262b1583b8691066da5b265a
Payton Swick <payton@foolord.com>
2020-04-14 18:20:40 -0400

    Bump version to 2.3

--- PhpcsChanged/functions.php
+++ PhpcsChanged/functions.php
@@ -8,7 +8,7 @@ use PhpcsChanged\PhpcsMessages;
 use PhpcsChanged\ShellException;
 
 function getVersion(): string {
-	return '2.2.7';
+	return '2.3.0';
 }
 
 function getNewPhpcsMessages(string $unifiedDiff, PhpcsMessages $oldPhpcsMessages, PhpcsMessages $newPhpcsMessages): PhpcsMessages {
