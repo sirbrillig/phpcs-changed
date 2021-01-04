45c6dcd64da45c2e54e173791f7a91737db106f2
Payton Swick <payton@foolord.com>
2021-01-04 13:43:32 -0500

    Bump version to 2.6

--- PhpcsChanged/functions.php
+++ PhpcsChanged/functions.php
@@ -8,7 +8,7 @@ use PhpcsChanged\PhpcsMessages;
 use PhpcsChanged\ShellException;
 
 function getVersion(): string {
-	return '2.5.0';
+	return '2.6.0';
 }
 
 function getNewPhpcsMessages(string $unifiedDiff, PhpcsMessages $oldPhpcsMessages, PhpcsMessages $newPhpcsMessages): PhpcsMessages {
