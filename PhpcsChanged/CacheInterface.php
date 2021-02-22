4ef8229a2b9b3a3168d7d10829efc250236b8175
Payton Swick <payton@foolord.com>
2021-02-22 14:02:50 -0500

    Refactor CacheInterface so it's just for load/save

--- PhpcsChanged/CacheInterface.php
+++ PhpcsChanged/CacheInterface.php
@@ -3,22 +3,10 @@ declare(strict_types=1);
 
 namespace PhpcsChanged\Cache;
 
+use PhpcsChanged\Cache\CacheManager;
+
 interface CacheInterface {
-	public function load(): void;
+	public function load(CacheManager $manager): void;
 
-	public function save(): void;
-
-	public function getRevision(): ?string;
-
-	public function getPhpcsStandard(): ?string;
-
-	public function setRevision(string $revisionId): void;
-
-	public function setPhpcsStandard(?string $standard): void;
-
-	public function getCacheForFile(string $filePath): ?string;
-
-	public function setCacheForFile(string $filePath, string $data): void;
-
-	public function clearCache(): void;
+	public function save(CacheManager $manager): void;
 }
