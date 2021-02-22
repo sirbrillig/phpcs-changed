57934606a62b10e9327b5f548d82b23b845b1160
Payton Swick <payton@foolord.com>
2021-02-22 14:04:56 -0500

    Refactor FileCache/TestCache to use CacheManager

--- PhpcsChanged/FileCache.php
+++ PhpcsChanged/FileCache.php
@@ -4,120 +4,45 @@ declare(strict_types=1);
 namespace PhpcsChanged\Cache;
 
 use PhpcsChanged\Cache\CacheInterface;
+use PhpcsChanged\Cache\CacheManager;
 
 define('DEFAULT_CACHE_FILE', '.phpcs-changed-cache');
 
 class FileCache implements CacheInterface {
 	/**
-	 * @var array<string, string>
-	 */
-	private $fileDataByPath = [];
-
-	/**
-	 * @var string
-	 */
-	private $revisionId;
-
-	/**
-	 * @var string | null
-	 */
-	private $phpcsStandard;
-
-	/**
-	 * @var bool
-	 */
-	private $hasBeenModified = false;
-
-	/**
 	 * @var string
 	 */
 	public $cacheFilePath = DEFAULT_CACHE_FILE; // phpcs:ignore ImportDetection -- apparently ImportDetection does not understand constants
 
-	public function load(): void {
+	public function load(CacheManager $manager): void {
 		if (! file_exists($this->cacheFilePath)) {
 			return;
 		}
-		$lines = file($this->cacheFilePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-		// We need at least three lines to be valid: revision, standard, and one data point
-		if (! $lines || count($lines) < 3) {
-			return;
+		$contents = file_get_contents($this->cacheFilePath);
+		if ($contents === false) {
+			throw new \Exception('Failed to read cache file');
 		}
-
-		// The first line is the revision id
-		$this->revisionId = $lines[0];
-
-		// The second line is the phpcs standard
-		$this->phpcsStandard = $lines[1];
-
-		// Further lines are key value pairs of the form `path/to/my file.php\n{"json_data": "goes here"}`, each one on two lines
-		for($lineNumber = 2; $line = $lines[$lineNumber] ?? null; $lineNumber += 2) {
-			if (! $line) {
-				break;
+		$decoded = json_decode($contents, true, 512, JSON_THROW_ON_ERROR);
+		if (! is_array($decoded) || ! array_key_exists('revisionId', $decoded) || ! array_key_exists('entries', $decoded) || ! is_array($decoded['entries'])) {
+			throw new \Exception('Invalid cache file');
+		}
+		$manager->setRevision($decoded['revisionId']);
+		foreach($decoded['entries'] as $entry) {
+			if (! array_key_exists('path', $entry) || ! array_key_exists('data', $entry) || ! array_key_exists('phpcsStandard', $entry)) {
+				throw new \Exception('Invalid cache file entry: ' . $entry);
 			}
-			$fileData = $lines[$lineNumber + 1] ?? null;
-			if (! $fileData) {
-				break;
-			}
-			$this->fileDataByPath[$line] = $fileData;
+			$manager->setCacheForFile($entry['path'], $entry['data'], $entry['phpcsStandard']);
 		}
 	}
 
-	public function save(): void {
-		if (! $this->hasBeenModified) {
-			return;
-		}
+	public function save(CacheManager $manager): void {
 		$data = [
-			$this->revisionId,
-			$this->phpcsStandard,
+			'revisionId' => $manager->getRevision(),
+			'entries' => $manager->getEntries(),
 		];
-		foreach($this->fileDataByPath as $filePath => $fileData) {
-			$data[] = $filePath;
-			$data[] = $fileData;
-		}
-		$result = file_put_contents($this->cacheFilePath, implode(PHP_EOL, $data));
+		$result = file_put_contents($this->cacheFilePath, json_encode($data, JSON_THROW_ON_ERROR));
 		if ($result === false) {
 			throw new \Exception('Failed to write cache file');
 		}
-	}
-
-	public function getRevision(): ?string {
-		return $this->revisionId;
-	}
-
-	public function getPhpcsStandard(): ?string {
-		return $this->phpcsStandard;
-	}
-
-	public function setRevision(string $revisionId): void {
-		if ($this->revisionId === $revisionId) {
-			return;
-		}
-		$this->hasBeenModified = true;
-		$this->revisionId = $revisionId;
-		$this->clearCache();
-	}
-
-	public function setPhpcsStandard(?string $standard): void {
-		if ($this->phpcsStandard === $standard) {
-			return;
-		}
-		$this->hasBeenModified = true;
-		$this->phpcsStandard = $standard;
-		$this->clearCache();
-	}
-
-	public function getCacheForFile(string $filePath): ?string {
-		return $this->fileDataByPath[$filePath] ?? null;
-	}
-
-	public function setCacheForFile(string $filePath, string $data): void {
-		$this->hasBeenModified = true;
-		$this->fileDataByPath[$filePath] = $data;
-	}
-
-	public function clearCache(): void {
-		$this->hasBeenModified = true;
-		$this->fileDataByPath = [];
 	}
 }
--- tests/helpers/TestCache.php
+++ tests/helpers/TestCache.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 namespace PhpcsChangedTests;
 
 use PhpcsChanged\Cache\CacheInterface;
+use PhpcsChanged\Cache\CacheManager;
 
 class TestCache implements CacheInterface {
 	/**
-	 * @var array<string, string>
+	 * @var array
 	 */
-	public $fileDataByPath = [];
+	private $fileData = [];
 
 	/**
 	 * @var string
@@ -17,67 +18,31 @@ class TestCache implements CacheInterface {
 	private $revisionId;
 
 	/**
-	 * @var string | null
-	 */
-	private $phpcsStandard;
-
-	/**
-	 * @var bool
-	 */
-	private $hasBeenModified = false;
-
-	/**
 	 * @var bool
 	 */
 	public $didSave = false;
 
-	public function load(): void {
+	public function load(CacheManager $manager): void {
+		$this->didSave = false;
+		$manager->setRevision($this->revisionId);
+		foreach($this->fileData as $entry) {
+			$manager->setCacheForFile($entry['path'], $entry['data'], $entry['phpcsStandard']);
+		}
 	}
 
-	public function save(): void {
-		if (! $this->hasBeenModified) {
-			return;
-		}
+	public function save(CacheManager $manager): void { // phpcs:ignore VariableAnalysis
 		$this->didSave = true;
 	}
 
-	public function getRevision(): ?string {
-		return $this->revisionId;
-	}
-
-	public function getPhpcsStandard(): ?string {
-		return $this->phpcsStandard;
+	public function setEntry(string $path, string $data, string $phpcsStandard): void {
+		$this->fileData[] = [
+			'path' => $path,
+			'data' => $data,
+			'phpcsStandard' => $phpcsStandard,
+		];
 	}
 
 	public function setRevision(string $revisionId): void {
-		if ($this->revisionId === $revisionId) {
-			return;
-		}
-		$this->hasBeenModified = true;
 		$this->revisionId = $revisionId;
-		$this->clearCache();
-	}
-
-	public function setPhpcsStandard(?string $standard): void {
-		if ($this->phpcsStandard === $standard) {
-			return;
-		}
-		$this->hasBeenModified = true;
-		$this->phpcsStandard = $standard;
-		$this->clearCache();
-	}
-
-	public function getCacheForFile(string $filePath): ?string {
-		return $this->fileDataByPath[$filePath] ?? null;
-	}
-
-	public function setCacheForFile(string $filePath, string $data): void {
-		$this->hasBeenModified = true;
-		$this->fileDataByPath[$filePath] = $data;
-	}
-
-	public function clearCache(): void {
-		$this->hasBeenModified = true;
-		$this->fileDataByPath = [];
 	}
 }
