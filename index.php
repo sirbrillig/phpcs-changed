4e02007ae371434481908286abb93fa639111073
Payton Swick <payton@foolord.com>
2021-02-22 14:05:30 -0500

    Include new files

--- index.php
+++ index.php
@@ -17,7 +17,9 @@ require_once __DIR__ . '/PhpcsChanged/NoChangesException.php';
 require_once __DIR__ . '/PhpcsChanged/ShellException.php';
 require_once __DIR__ . '/PhpcsChanged/ShellOperator.php';
 require_once __DIR__ . '/PhpcsChanged/UnixShell.php';
+require_once __DIR__ . '/PhpcsChanged/CacheEntry.php';
 require_once __DIR__ . '/PhpcsChanged/CacheInterface.php';
+require_once __DIR__ . '/PhpcsChanged/CacheManager.php';
 require_once __DIR__ . '/PhpcsChanged/FileCache.php';
 
 // Function-only files
