4b6bd85336c9c7fefafaa62db0f8a77a11debafe
Payton Swick <payton@foolord.com>
2024-05-23 15:57:44 -0400

    Bump version to 2.11.5

--- PhpcsChanged/functions.php
+++ PhpcsChanged/functions.php
@@ -7,7 +7,7 @@ use PhpcsChanged\PhpcsMessages;
 use PhpcsChanged\ShellException;
 
 function getVersion(): string {
-	return '2.11.4';
+	return '2.11.5';
 }
 
 function getNewPhpcsMessages(string $unifiedDiff, PhpcsMessages $unmodifiedPhpcsMessages, PhpcsMessages $modifiedPhpcsMessages): PhpcsMessages {
